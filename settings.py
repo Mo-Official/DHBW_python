1a54bd62346768f5d155ffa4ae549e990c2f9a0a
Mo-Official <inf19151@lehre.dhbw-stuttgart.de>
2021-04-08 23:57:27 +0200

    added additional constants

--- settings.py
+++ settings.py
@@ -1,15 +1,38 @@
 import os
 
 # system stats
-game_path = os.path.dirname(__file__)
-assets_path = os.path.join(game_path, "assets")
+GAME_PATH = os.path.dirname(__file__)
+ASSETS_PATH = os.path.join(GAME_PATH, "assets")
+HS_FILE = os.path.join(GAME_PATH, "HighScore.txt")
 
 # window stats
 TITLE = "My Title"
 WIDTH = 1024
 HEIGHT = 640
 TITLE = "My Game"
-FPS = 30
+FPS = 60
+FONT_ARIAL = "arial"
+
+# Player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+PLAYER_GRAVITY = 0.8
+PLAYER_JUMP = -20
+
+# Starting Platforms
+PLATFORM_LIST = [
+    (0, HEIGHT - 40 , WIDTH, 40),
+    (WIDTH/2 - 200, HEIGHT * 3/4 - 50, 100, 20),
+    (125, HEIGHT - 350, 100, 20),
+    (350, 200, 100, 20),
+    (175, 100, 50, 20)]
+
+COIN_LIST = [
+    (50, HEIGHT-100, 100),
+    (125, HEIGHT - 400, 75),
+    (350, 250, 25)
+]
+
 
 # colors
 WHITE = (255,255,255)
@@ -21,3 +44,6 @@ RED = (255,0,0)
 BROWN = (160,82,45)
 CYAN = (224,255,255)
 YELLOW = (255,255,0)
+LIGHTBLUE = (0, 155, 155)
+BG_COLOR = LIGHTBLUE
+
