a34c8fd961964fdf74abba527f210df772823bc9
Mo-Offical <inf19151@lehre.dhbw-stuttgart.de>
2021-06-06 20:36:51 +0200

    added test cases for util.py

--- util.py
+++ util.py
@@ -27,6 +27,23 @@ __doc__ = """
 """
 
 def print_log(msg, mode="INFO"):
+    """A wrapper function around print() and open().write()
+    
+    Parameters
+    ----------
+    msg: the string that should be written to the file,
+    or logged to the console
+    
+    mode: a tag the is appended to the message.
+    useful when handeling logs messages differently.
+
+    Tests
+    -----
+    - passing none string objects as a msg
+    - open("log.txt", "a") no having write permissions
+    - if a global variable is missing
+    
+    """
     if not LOG_INFO and mode=="INFO":
         return
 
@@ -48,7 +65,16 @@ def print_log(msg, mode="INFO"):
 def debug(func):
     """
     Print the function signature and returns value
-    SOURCE: https://realpython.com/primer-on-python-decorators/#debugging-code"""
+    SOURCE: https://realpython.com/primer-on-python-decorators/#debugging-code
+    
+    Parameters
+    ----------
+    func: the function that is wrapped by the debug decorator
+
+    Test
+    ----
+    general error.
+    """
     @functools.wraps(func)
     def wrapper_debug(*args, **kwargs):
         args_repr = [repr(a) for a in args]                      # 1
@@ -72,6 +98,18 @@ def get_outline(image: Surface, color: Tuple[int,int,int]=RED)->Surface:
     color is the color which the outline will be drawn.
     
     SOURCE: https://pastebin.com/XXRngMZh
+
+    Parameter
+    ---------
+    image: image to calculate its mask
+
+    color: color of the calculated mask
+
+    Test
+    ----
+    passing a none-surface as image
+    passing a none-tuple as color
+    passing a invalid color
     """
     rect = image.get_rect()
     mask = pg.mask.from_surface(image)
